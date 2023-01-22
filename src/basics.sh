26682c1e658e7c35b2f27089ef994aa296897729
sharpordie <sharpordie@outlook.com>
2023-01-22 16:54:31 +0100

    small update

--- src/basics.sh
+++ src/basics.sh
@@ -15,11 +15,9 @@ expand_version() {
     local default=${2:-0.0.0.0}
 
     brew install grep
-
     local starter=$(expand_pattern "$payload/*ontents/*nfo.plist")
     local version=$(defaults read "$starter" CFBundleShortVersionString 2>/dev/null)
-    local version=$(echo $version | ggrep -oP "[\d.]+" || echo "$default")
-
+    local version=$(echo "$version" | ggrep -oP "[\d.]+" || echo "$default")
     echo $version
 
 }
@@ -27,32 +25,24 @@ expand_version() {
 update_dbeaver_ultimate() {
 
     brew install curl jq
-
     local address="https://dbeaver.com/download/ultimate/"
     local pattern="DBeaver Ultimate Edition \K([\d.]+)"
     local version=$(curl -LA "mozilla/5.0" "$address" | ggrep -oP "$pattern" | head -1)
     local current=$(expand_version "/*ppl*/*eav*lti*")
-
     autoload is-at-least
-    local updated=$(is-at-least "$version" "$current" && echo true || echo false)
-
-    if [[ $updated == false ]]; then
-
+    local updated=$(is-at-least "$version" "$current" && echo "true" || echo "false")
+    if [[ "$updated" == "false" ]]; then
 	    local cpuname=$(sysctl -n machdep.cpu.brand_string)
-	    local silicon=$([[ $cpuname =~ pple ]] && echo true || echo false)
-	    local adjunct=$([[ $silicon == true ]] && echo aarch64 || echo x86_64)
+	    local silicon=$([[ $cpuname =~ "pple" ]] && echo "true" || echo "false")
+	    local adjunct=$([[ $silicon == "true" ]] && echo "aarch64" || echo "x86_64")
         local address="https://download.dbeaver.com/ultimate/$version.0/dbeaver-ue-$version.0-macos-$adjunct.dmg"
-
 	    local fetched="$(mktemp -d)/$(basename "$address")"
 		curl -LA "mozilla/5.0" "$address" -o "$fetched"
-
         hdiutil convert "$fetched" -format UDTO -o "$fetched.cdr"
 		hdiutil attach "$fetched.cdr" -noautoopen -nobrowse
 		cp -fr /Volumes/DB*/DB*.app /Applications
 		hdiutil detach /Volumes/DB*
-
 		sudo xattr -rd com.apple.quarantine /*ppl*/*eav*lti*
-
     fi
 
 }
