299e171eca19d235e3a25e5e7c841a334a7862c5
sharpordie <sharpordie@outlook.com>
2023-01-23 10:10:07 +0100

    small update

--- src/basics.sh
+++ src/basics.sh
@@ -15,16 +15,17 @@ expand_version() {
     local default=${2:-0.0.0.0}
 
     brew install grep
+
     local starter=$(expand_pattern "$payload/*ontents/*nfo.plist")
     local version=$(defaults read "$starter" CFBundleShortVersionString 2>/dev/null)
-    local version=$(echo "$version" | ggrep -oP "[\d.]+" || echo "$default")
-    echo $version
+    echo "$version" | ggrep -oP "[\d.]+" || echo "$default"
 
 }
 
 update_dbeaver_ultimate() {
 
     brew install curl jq
+
     local address="https://dbeaver.com/download/ultimate/"
     local pattern="DBeaver Ultimate Edition \K([\d.]+)"
     local version=$(curl -LA "mozilla/5.0" "$address" | ggrep -oP "$pattern" | head -1)
@@ -47,4 +48,29 @@ update_dbeaver_ultimate() {
 
 }
 
-update_dbeaver_ultimate
+update_docker_desktop() {
+
+    brew install curl jq
+    
+    local address="https://docs.docker.com/desktop/release-notes/"
+    local pattern="<h2 id=\"[\d]+\">\K([\d.]+)"
+    local version=$(curl -LA "mozilla/5.0" "$address" | ggrep -oP "$pattern" | head -1)
+    local current=$(expand_version "/*ppl*/*ocke*")
+    autoload is-at-least
+    local updated=$(is-at-least "$version" "$current" && echo "true" || echo "false")
+    if [[ "$updated" == "false" ]]; then
+	    local cpuname=$(sysctl -n machdep.cpu.brand_string)
+	    local silicon=$([[ $cpuname =~ "pple" ]] && echo "true" || echo "false")
+	    local adjunct=$([[ $silicon == "true" ]] && echo "arm64" || echo "amd64")
+        local address="https://desktop.docker.com/mac/main/$adjunct/Docker.dmg"
+	    local fetched="$(mktemp -d)/$(basename "$address")"
+        curl -LA "mozilla/5.0" "$address" -o "$fetched"
+		sudo hdiutil attach "$fetched"
+        sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license --user=$USER
+        sudo hdiutil detach /Volumes/Docker
+    fi
+
+}
+
+sudo -v
+update_docker_desktop
